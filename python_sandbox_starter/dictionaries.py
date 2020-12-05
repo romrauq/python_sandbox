0a45d8e050631c4b26c0c8d4f1d5c6cb7250b0a1
John Quarmor <johnequarmor@gmail.com>
2020-12-05 23:33:56 +0000

    Deleted starter files, and replaced starter files (only modified file is variables.py)

--- python_sandbox_starter/dictionaries.py
+++
@@ -1,2 +0,0 @@
-# A Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
-# Read more about dictionaries at https://docs.python.org/3/tutorial/datastructures.html#dictionaries
